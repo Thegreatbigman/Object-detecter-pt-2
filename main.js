a92cf45def37bfce077426d0a26761a5607a3c3e
Thegreatbigman <113654502+Thegreatbigman@users.noreply.github.com>
2023-04-27 18:34:55 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function preload(){
+    img = loadImage('bed room.jpg');
+  }
+
+  function setup() {
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+  }
